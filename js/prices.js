da16709c331a75a30ed58229fabe9a6d8270e2ce
Roi Hagag <roihagag@yahoo.com>
2018-10-01 19:08:19 +0300

    added delivery choosing

--- js/prices.js
+++ js/prices.js
@@ -1,16 +1,26 @@
 function getPrice(imgId){
-    var thirties = ['6', '7', '8', '9', '12', '13', '14', '17'];
-    var thrirtyFives = ['4', '10', '11', '16', '19'];
-    var fourtees = ['3', '5', '15', '18'];
-    var fourtyFives = ['1', '2'];
+    var twentyfives = ['6', '7', '8', '9', '12', '13', '14', '17'];
+    var thirtyies = ['4', '10', '11', '16', '19'];
+    var thirtyfives = ['3', '5', '15', '18'];
+    var fourties = ['1', '2'];
 
-    if (thirties.indexOf(imgId) != -1)
+    if (twentyfives.indexOf(imgId) != -1)
+    {
+        return '25';
+    }
+
+    if (thirtyies.indexOf(imgId) != -1)
+    {
         return '30';
-    if (thrirtyFives.indexOf(imgId) != -1)
-    return '35';
-    if (fourtees.indexOf(imgId) != -1)
-    return '40';
-    if (fourtyFives.indexOf(imgId) != -1)
-    return '45';
+    }
 
+    if (thirtyfives.indexOf(imgId) != -1)
+    {
+        return '35';
+    }
+
+    if (fourties.indexOf(imgId) != -1)
+    {
+        return '40';
+    }
 }
